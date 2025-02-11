6b1b3094ff26f23c447b411b65585d7725f82e49
Aiman Qadree <aimanqadree@gmail.com>
2025-02-11 10:17:11 +0530

    Project Setup

--- src/Model/Landing/Review.jsx
+++ src/Model/Landing/Review.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
- function Review() {
+import styles from './landing.module.css'
+function Review() {
   return (
-    <div>Review</div>
+    <div className= {styles.top}>
+      <h1 className={styles.headingreview}>Find the career you deserve</h1>
+
+      <div className={styles.parentreview}>
+
+        <div className={styles.box1}>
+          <p className={styles.para}> "SkillMatch is user-friendly and has a vast database of job listings. However, Sometimes it can be challenging to get in touch with recruiters directly."</p>
+            <h4 className={styles.h4review}>- Abishek Newar</h4>
+        </div>
+
+        <div className={styles.box1}>
+          <p className={styles.para}>"SkillMatch is excellent for salary insights and company reviews. It helps set realistic salary expectations but lacks some entry-level job listings."</p>
+          <h4 className={styles.h4review}>- Aiman Jan</h4>
+        </div>
+
+        <div className={styles.box1}>
+          <p className={styles.para}>"SkillMatch offers a streamlined application process and excellent job matching algorithms. It's great for finding jobs."</p>
+          <h4 className={styles.h4review}>- Hiten Yadav</h4>
+        </div>
+
+      </div>
+    </div>
   )
 }
 export default Review
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import styles from  "./navbar.module.css"
-
+import {useNavigate} from "react-router-dom"
  function Navbar() {
+  const navigate = useNavigate()
   return (
     <div>
       <nav className={styles.Navbar}>
         <div>
           <h3 className={styles.logo}>SKILLMATCH</h3>
         </div>
-        <button className={styles.btn}>Login</button>
+        <button onClick={()=>{navigate("/auth")}} className={styles.btn}>Login</button>
       </nav>
     </div>
   )
