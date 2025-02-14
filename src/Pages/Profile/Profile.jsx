d1870933cb91a0f115f06f5ff4d1cebf245c0f20
Aiman Qadree <aimanqadree@gmail.com>
2025-02-14 11:08:59 +0530

    setup Project

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -4,34 +4,37 @@ import styles from './Profile.module.css'
 function Profile() {
   return (
     <div className={styles.container}>
-      <div className={styles.headingimage}>
-        <img className={styles.img} src="imageaiman.jpg" alt="Profile" />
+
+      <div className={styles.box1}>
+        <img className={styles.img} src="imageaimann.jpg" alt="Profile" />
       </div>
-      
-          <div>
-      <div className={styles.details}>
-        <h1>Aiman Jan</h1>
-        <h4>Frontend Developer</h4>
-        <p>at Infotechmon Pvt. Ltd</p>
+
+      <div className={styles.box2}>
+
+        <div className={styles.details}>
+          <h1>Aiman Jan</h1>
+          <h4>Frontend Developer</h4>
+          <p>at Infotechmon Pvt. Ltd</p>
         </div>
 
-      <div>
-        <ul>
-          <li>Mohali,INDIA</li>
-          <li>Fresher</li>
-          <li>2,0400</li>
-        </ul>
+        <div className= {styles.location}>
 
-        <ul>
-          <li>7006110177</li>
-          <li>aimanQadree@gmail.com</li>
-          <li>15 Days Or less</li>
-        </ul>
+          <ul className={styles.lists}>
+            <li>Mohali,INDIA</li>
+            <li>Fresher</li>
+            <li>2,04000</li>
+          </ul>
+
+          <ul className={styles.lists}>
+            <li>7006110177</li>
+            <li>aimanQadree@gmail.com</li>
+            <li>15 Days Or less</li>
+          </ul>
         </div>
-    </div>
+      </div>
     </div>
   )
 }
-export default  Profile
+export default Profile
 
 
