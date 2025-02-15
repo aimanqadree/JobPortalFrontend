c06c9de20feb151becb2fdf62c5562e5498bcf6d
Aiman Qadree <aimanqadree@gmail.com>
2025-02-15 10:06:37 +0530

    setup project

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import styles from './Profile.module.css'
+import { IoLocationOutline } from "react-icons/io5";
+import { CiMail } from "react-icons/ci";
+import { IoCalendarClearOutline } from "react-icons/io5";
+import { CiPhone } from "react-icons/ci";
+import { PiSuitcaseSimple } from "react-icons/pi";
+import { MdMailOutline } from "react-icons/md";
+import { CiWallet } from "react-icons/ci";
+import { RxPencil1 } from "react-icons/rx";
 
 function Profile() {
   return (
+    <div className= {styles.main}>
     <div className={styles.container}>
 
       <div className={styles.box1}>
@@ -12,7 +21,7 @@ function Profile() {
       <div className={styles.box2}>
 
         <div className={styles.details}>
-          <h1>Aiman Jan</h1>
+          <h1>Aiman Jan <RxPencil1 className={styles.headinglogue} /></h1>
           <h4>Frontend Developer</h4>
           <p>at Infotechmon Pvt. Ltd</p>
         </div>
@@ -20,21 +29,86 @@ function Profile() {
         <div className= {styles.location}>
 
           <ul className={styles.lists}>
-            <li>Mohali,INDIA</li>
-            <li>Fresher</li>
-            <li>2,04000</li>
+            <li> <IoLocationOutline /> Mohali,INDIA</li>
+            <li> <PiSuitcaseSimple /> Fresher</li>
+            <li> <CiWallet />  ₹ 2,04000</li>
           </ul>
 
           <ul className={styles.lists}>
-            <li>7006110177</li>
-            <li>aimanQadree@gmail.com</li>
-            <li>15 Days Or less</li>
+            <li> <CiPhone /> 7006110177</li>
+            <li> <MdMailOutline />aimanQadree@gmail.com</li>
+            <li> <IoCalendarClearOutline />15 Days Or less</li>
           </ul>
         </div>
       </div>
     </div>
+    <New/>
+    </div>
   )
 }
 export default Profile
+
+function New(){
+  const object = [
+    {
+      name:"Resume",
+      link: "#"
+    },
+    {
+      name:"Resume headline",
+      link: "#"
+    },
+    {
+      name:"Key skills",
+      link: "#"
+    },
+    {
+      name:"Employment",
+      link: "#"
+    },
+    {
+      name:"Education",
+      link: "#"
+    },
+    {
+      name:"IT skills",
+      link: "#"
+    },
+    {
+      name:"Projects",
+      link: "#"
+    },
+    {
+      name:"Profile summary",
+      link: "#"
+    },
+    {
+      name:"Accomplishments",
+      link: "#"
+    },
+    {
+      name:"Career profile",
+      link: "#"
+    },
+    {
+      name:"Personal details",
+      link: "#"
+    }
+
+  ]
+  return(
+    <div className= {styles.secondcontainer}>
+    <h2 className={styles.heading2}>Quick Links</h2>
+    <div>
+        {
+          object.map((item,index)=>(
+            <div className={styles.boxes} key={index}>{item.name}</div>
+          ))
+        }
+    </div>
+    </div>
+  )
+}
+
 
 
